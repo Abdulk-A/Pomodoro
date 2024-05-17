a053e0a3a66f7b3a294bb7fa8fbd5eed977c99ff
Abdulk-A <abdullahabdulkareem273@gmail.com>
2024-05-16 19:35:16 -0500

    .

---
+++ Pomodoro/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by Abdullah Abdulkareem on 5/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
