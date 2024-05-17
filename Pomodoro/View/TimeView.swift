0ce5e22ac605155445539bfe865d7941a898ee5f
Abdulk-A <abdullahabdulkareem273@gmail.com>
2024-05-17 15:33:51 -0500

    Updated timer functionality and added break time

--- Pomodoro/View/TimeView.swift
+++ Pomodoro/View/TimeView.swift
@@ -13,34 +13,33 @@ struct TimeView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     @State private var isTimerPaused = true
-    
+    @State private var isBreak = false
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                
                 HStack {
                     Button(isTimerPaused ? "Start" : "Stop") {
                         withAnimation(.easeIn){
                             isTimerPaused.toggle()
                         }
                     }
-                        .padding()
-                        .frame(width: geometry.size.width*0.32)
-                        .background(isTimerPaused ? .green : .red)
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .padding()
+                    .frame(width: geometry.size.width*0.32)
+                    .background(isTimerPaused ? .green : .red)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
                     
                     Spacer()
                     Button("Reset") {
                         withAnimation{
                             restartTimer()
                         }
-
+                        
                     }
-                        .padding()
-                        .frame(width: geometry.size.width*0.32)
-                        .background(Color.orange)
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .padding()
+                    .frame(width: geometry.size.width*0.32)
+                    .background(Color.orange)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
                 }
                 .frame(maxWidth: geometry.size.width * 0.7)
                 .opacity(0.9)
@@ -57,6 +56,18 @@ struct TimeView: View {
                             }
                     }
                     .shadow(radius: 10)
+                    .padding(.bottom, 5)
+                Button(isBreak ? "Study" : "Break"){
+                    isBreak.toggle()
+                    timeRemaining = isBreak ? .BREAK_TIME : .STUDY_TIME
+                }
+                    .font(.largeTitle)
+                    .padding(5)
+                    .frame(width: geometry.size.width * 0.70)
+                    .background(isBreak ? Color.Anti_flash_white : Color.tomato)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .foregroundStyle(isBreak ? Color.tomato : Color.Anti_flash_white).opacity(0.9)
+                    .shadow(radius: 3)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .foregroundStyle(Color.onyx)
@@ -65,7 +76,7 @@ struct TimeView: View {
     
     func restartTimer() {
         isTimerPaused = true
-        timeRemaining = .STUDY_TIME
+        timeRemaining = isBreak ? .BREAK_TIME : .STUDY_TIME
     }
     func pauseTimer() {
         isTimerPaused = false
@@ -73,7 +84,12 @@ struct TimeView: View {
     
     func updateTimer(){
         
-        if timeRemaining > 0  && !isTimerPaused {
+        if timeRemaining == 0 {
+            timeRemaining = isBreak ? .STUDY_TIME : .BREAK_TIME
+            isBreak.toggle()
+            isTimerPaused = true
+        }
+        else if timeRemaining > 0  && !isTimerPaused {
             timeRemaining -= 1
         }
 
